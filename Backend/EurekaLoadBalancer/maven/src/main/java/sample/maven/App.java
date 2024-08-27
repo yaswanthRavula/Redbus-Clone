8e3e5a27dd65d4eb85c547f4126821a3df924277
Yaswanth Ravula <yaswanthravula0@gmail.com>
2024-08-28 00:32:34 +0530

    POM updated dependencies changed

--- Backend/EurekaLoadBalancer/maven/src/main/java/sample/maven/App.java
+++ Backend/EurekaLoadBalancer/maven/src/main/java/sample/maven/App.java
@@ -1,10 +1,14 @@
 package sample.maven;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -18,12 +22,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class);
+        new SpringApplicationBuilder(App.class)
+        .web(WebApplicationType.NONE).run(args);
     }
-    
+
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate() {
+    RestTemplate restTemplate() {
     	return new RestTemplate();
     }
 }

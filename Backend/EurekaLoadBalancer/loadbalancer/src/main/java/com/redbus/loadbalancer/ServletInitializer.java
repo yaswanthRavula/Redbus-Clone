64461ded2eea3d9c4420a465ec6770c79c81c82e
Yaswanth Ravula <yaswanthravula0@gmail.com>
2024-08-28 22:19:04 +0530

    Loadbalancer entire spring project re inistialised and configured

---
+++ Backend/EurekaLoadBalancer/loadbalancer/src/main/java/com/redbus/loadbalancer/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.redbus.loadbalancer;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LoadbalancerApplication.class);
+	}
+
+}
---
+++ Backend/EurekaLoadBalancer/loadbalancer/src/main/java/com/redbus/loadbalancer/controllers/Controller.java
@@ -0,0 +1,23 @@
+package com.redbus.loadbalancer.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("path")
+public class Controller {
+
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@GetMapping("/test")
+	public String testEndPoint() {
+		String reString = "http://myService/seats/API-LB-instance";
+		return restTemplate.getForEntity(reString, String.class).getBody();
+	}
+	
+	
+}
